7b3796e15d0ffc135c2708b948a232d9602fb16b
jtrancoso-alt <117353028+jtrancoso-alt@users.noreply.github.com>
2025-04-22 20:54:37 +0200

    headers

--- main.py
+++ main.py
@@ -21,11 +21,12 @@ SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
 
 def get_rss_feed(url):
     headers = {
-        "User-Agent": "Mozilla/5.0",
-        "Accept": "application/rss+xml,application/xml",
-        "Referer": "https://www.loteriasyapuestas.es/"
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.15; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.4 Safari/605.1.15",
+    "Accept": "application/rss+xml,application/xml",
+    "Referer": "https://www.loteriasyapuestas.es/"
     }
-    response = requests.get(url, headers=headers)
+    
+    response = requests.get(url, headers=headers, timeout=10)
     response.raise_for_status()
 
     soup = BeautifulSoup(response.text, "xml")
